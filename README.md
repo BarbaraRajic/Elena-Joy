60875337295c918090f4a7f54d6c68128897df1f
Barbara Rajič <barbara.rajic@yahoo.com>
2022-11-15 17:10:09 +0100

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Professional Photographer Portfolio Website
+# Elena Joy - Professional Photographer Portfolio Website
+
+### Description
+
+* Simple, responsive landing page
+
+### Screenshots
+
+Navbar and hero sections
+![Elena Joy](/img/elena-joy-screenshot.png)
+
+### Development
+
+* Built with: CSS, HTML and with a little help from JavaScript
--- app.js
+++ app.js
@@ -1,16 +1,62 @@
-const nav = document.querySelector(".nav-links");
-const links = nav.querySelectorAll("a");
+const dom = (() => {
+  const nav = document.querySelector(".nav-links");
+  const main = document.querySelector("main");
+  const footer = document.querySelector("footer");
+  const burger = document.querySelector(".burger");
+  const links = nav.querySelectorAll("a");
 
-const burger = document.querySelector(".burger");
+  // Helpers
+  const hide = () => {
+    dom.nav.style.display = "none"
+    dom.main.classList.remove("hidden");
+    dom.footer.classList.remove("hidden");
+  }
+  
+  const show = () => {
+    dom.nav.style.display = "flex"
+    dom.main.classList.add("hidden");
+    dom.footer.classList.add("hidden");
+  }
+  
+  const toggleHere = () => {
+    return (/toggle/i).test(dom.burger.className);;
+  }
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-open");
-  burger.classList.toggle("toggle");
-});
+  return {
+    nav,
+    links,
+    main,
+    footer, 
+    burger,
+    hide,
+    show,
+    toggleHere,
+  }
+})();
 
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    nav.classList.toggle("nav-open");
-    burger.classList.toggle("toggle");
-  });
-});
+const controller = ((DOM) => {
+
+  const setEventListeners = () =>{
+    DOM.burger.addEventListener("click", () => {
+      DOM.burger.classList.toggle("toggle");
+     
+      DOM.toggleHere() ? DOM.show() : DOM.hide();
+    });
+    
+    DOM.links.forEach((link) => {
+      link.addEventListener("click", () => {
+        if(DOM.toggleHere()){ 
+          DOM.hide();
+          DOM.burger.classList.toggle("toggle");
+        }
+      });
+    });
+  } 
+  
+  return {
+    setEventListeners,
+  }
+})(dom);
+
+controller.setEventListeners();
+
